b37af5ab4e029f0113063465b4fa13d532524294
sergei-doroshenko <6823298@gmail.com>
2016-02-20 00:18:01 +0300

    create in one gorutine

--- main.go
+++ main.go
@@ -8,35 +8,26 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
 
 const url string = "http://www.webservicex.net/globalweather.asmx/GetCitiesByCountry?CountryName="
 
-type Table struct {
-	XMLName xml.Name `xml: "Table"`
-	Country string   `xml:"Country"`
-	City    string   `xml: City"`
+type XmlRecord struct {
+	XMLName    xml.Name `xml: "string"`
+	NewDataSet struct {
+		XMLName xml.Name `xml: "NewDataSet"`
+		Table   []struct {
+			XMLName xml.Name `xml: "Table"`
+			Country string   `xml:"Country"`
+			City    string   `xml: City"`
+		} `xml: "Table"`
+	} `xml: "NewDataSet"`
 }
 
-type NewDataSet struct {
-	XMLName xml.Name `xml: "NewDataSet"`
-	Table   []Table  `xml: "Table"`
-}
-
-type Root struct {
-	XMLName    xml.Name   `xml: "string"`
-	NewDataSet NewDataSet `xml: "NewDataSet"`
-}
-
-/*
 type LogRecord struct {
 	Country string   `json:"country"`
 	Cities  []string `json:"cities"`
-}
-*/
-type LogRecord struct {
-	Country string
-	Cities  []string
 }
 
 func getXml(country string) string {
@@ -57,56 +48,77 @@ func getXml(country string) string {
 	return html.UnescapeString(string(contents))
 }
 
-func createLogFile(filename string) *os.File {
-	f, err3 := os.Create("test.log")
+func convertToJson(xmlRecord XmlRecord) string {
+	records := xmlRecord.NewDataSet.Table
+	if len(records) > 0 {
+		var c string = xmlRecord.NewDataSet.Table[0].Country
+
+		var ct []string = make([]string, 0)
+
+		for _, element := range xmlRecord.NewDataSet.Table {
+			// element is the element from someSlice for where we are
+			ct = append(ct, element.City)
+		}
+
+		res2D := LogRecord{
+			Country: c,
+			Cities:  ct}
+
+		jsonData, _ := json.Marshal(res2D)
+		return string(jsonData)
+	}
+	return ""
+}
+
+func createLogFile(filename string) {
+	_, err3 := os.Create(filename)
 	if err3 != nil {
 		fmt.Println(err3.Error())
+	} else {
+		fmt.Println("Created file: ", filename)
 	}
-
-	fmt.Println("Created file: ", f.Name)
-	return f
 }
 
 func appendToFile(filename, text string) {
-	/*
-		var logfile *os.File
-		_, err4 := os.Stat(filename)
-		if err4 != nil {
-			fmt.Println(err4.Error())
-			fmt.Println("Log file: " + filename + " doesn't exists.")
-			logfile = createLogFile(filename)
-			logfile.Chmod(0644)
-			defer logfile.Close()
-		}
-	*/
-	/*
-		f := createLogFile(filename)
-		defer f.Close()
-	*/
-	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0600)
+
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		// path/to/whatever does not exist
+		fmt.Println("File not exists")
+		createLogFile(filename)
+	}
+
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0600) //0644
 	if err != nil {
 		// panic(err)
 		fmt.Println("File doesn't exist")
-		fmt.Println(err.Error())
+		fmt.Println("Open file error: ", err.Error())
+
+	} else {
+		fmt.Println(f.Name())
+
+		if _, err = f.WriteString(text + "\n"); err != nil {
+			//panic(err)
+			fmt.Println("Can't write to file")
+			fmt.Println(err.Error())
+		}
 
 	}
 
 	defer f.Close()
-	fmt.Println(f.Name())
+}
 
-	if _, err = f.WriteString(text); err != nil {
-		//panic(err)
-		fmt.Println(err.Error())
-	}
-
+func logToFile(text string) {
+	current_time := time.Now().Local()
+	filename := "cities-" + current_time.Format("2006-01-02") + ".log"
+	appendToFile(filename, text)
 }
 
 func main() {
 	fmt.Println("Hello, start process...")
 	xmlData := getXml("Ukrain")
-	fmt.Println(xmlData)
-	v := Root{}
+	// fmt.Println(xmlData)
 
+	v := XmlRecord{}
 	err1 := xml.Unmarshal([]byte(xmlData), &v)
 
 	if err1 != nil {
@@ -114,31 +126,8 @@ func main() {
 		return
 	}
 
-	fmt.Println("XMLName: ", v.XMLName)
-	fmt.Println(v.NewDataSet.Table)
-	res1D := LogRecord{
-		Country: "USA",
-		Cities:  []string{"New York", "Boston", "Dallas"}}
+	json := convertToJson(v)
+	// fmt.Println("Converted json: ", json)
 
-	res1B, _ := json.Marshal(res1D)
-	fmt.Println(string(res1B))
-
-	logRecord := LogRecord{Country: v.NewDataSet.Table[0].Country, Cities: make([]string, 0)}
-	for _, element := range v.NewDataSet.Table {
-		// element is the element from someSlice for where we are
-		logRecord.Cities = append(logRecord.Cities, element.City)
-	}
-	fmt.Println(logRecord)
-	/*
-		logRecord := LogRecord{
-			Country: "GreateBritain",
-			Cities:  []string{"Glazgo", "London", "Birmengerm"}}
-		fmt.Println(logRecord)
-	*/
-	//logStr, _ := json.Marshal(LogRecord)
-	//fmt.Println(string(logStr))
-
-	//	appendToFile("./test.log", logStr)
-
-	//ioutil.WriteFile("log.txt", []byte(xmlData), 0644)
+	logToFile(json)
 }
